d0d043357e9ee485572ff2d45f9e9a3e79d72a92
udaym9651 <uday.m9651@gmail.com>
2022-05-22 21:19:01 +0530

    Update loadscriptfile.groovy

--- vars/loadscriptfile.groovy
+++ vars/loadscriptfile.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]){
   def scriptcontents = libraryResource "${config.name}"
-  WriteFile file: "${config.name}" , text: scriptcontents
+  writeFile file: "${config.name}" , text: scriptcontents
   sh "chmod a+x ./${config.name}"
 }
